5828e7e9bffe9f7fee186c4754b61c30fc1afa89
EvoneeHsieh <evonee.hsieh@hotmail.com>
2025-09-13 18:46:21 +0800

    puzzle Snap Animation complete
    snap animation complete
    add snap distant
    
    *the reason why I can't success just because the grid one and puzzle's collider, one use is trigger and the other isn't

--- Assets/Scripts/puzzle/Homework.cs
+++ Assets/Scripts/puzzle/Homework.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Homework : MonoBehaviour
 {
     [Header("Snap")]
     public bool isStuck = false;
+    public bool canDrag = true;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        if (isStuck) return;
+
         if (collision.gameObject.CompareTag(this.tag))
         {
             Snapping(collision.transform);
@@ -18,21 +22,43 @@ public class Homework : MonoBehaviour
     void Snapping(Transform target)
     {
         isStuck = true;
-        transform.position = target.position;
-        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-        GetComponent<Collider2D>().enabled = false;
-        transform.SetParent(target);
+        canDrag = false;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Collider2D col = GetComponent<Collider2D>();
+
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.freezeRotation = true;
+
+        col.enabled = false;
+
+        // snapping animation
+        LeanTween.move(gameObject, target.position, 0.3f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => { Debug.Log("snapAnimation"); });
+        StartCoroutine(SetParentAfterDelay(target, 0.3f));
     }
+
 
     //·Æ¹«©ì¦²
     void OnMouseDrag()
     {
-        transform.position = getMousePos();
+        if (!isStuck && canDrag)
+        {
+            transform.position = GetMousePos();
+        }
     }
-    Vector3 getMousePos()
+    Vector3 GetMousePos()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         return mousePos;
     }
+
+    IEnumerator SetParentAfterDelay(Transform target, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        transform.SetParent(target);
+    }
+
 }
